eb1c83556d14f87a606e1a790f0395ac031eabde
baimamboukar <baimamboukar@gmail.com>
2023-01-17 19:24:32 +0100

    CosmoSense 1.0

---
+++ lib/src/router/router.dart
@@ -0,0 +1,24 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cosmosense/main.dart';
+import 'package:cosmosense/src/ui/screens/screens.dart';
+
+@AdaptiveAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    AutoRoute(
+      page: Home,
+      path: '/',
+      initial: true,
+      children: [
+        AutoRoute(page: Cosmos, path: 'cosmos'),
+        AutoRoute(page: NasaImage, path: 'image-of-the-day'),
+        AutoRoute(page: Exploration, path: 'exploration'),
+        AutoRoute(page: Weather, path: 'space-weather'),
+      ],
+    ),
+    AutoRoute(page: Login, path: '/login'),
+    AutoRoute(page: Signin, path: '/signin'),
+    AutoRoute(page: Welcome, path: '/welcome')
+  ],
+)
+class $AppRouter {}
