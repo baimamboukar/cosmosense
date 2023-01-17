9db07bbbc6e8026508182dbc09b5e7c7d08c8461
baimamboukar <baimamboukar@gmail.com>
2023-01-17 19:24:32 +0100

    CosmoSense 1.0

---
+++ lib/src/data/models/planet.dart
@@ -0,0 +1,30 @@
+class Planet {
+  final String name;
+  final String image;
+  final String history;
+
+  Planet({required this.name, required this.image, required this.history});
+}
+
+final planets = [
+  Planet(
+      image: "assets/images/planetred.png",
+      name: "MARS",
+      history: "Lorem ipusm dolor si amet"),
+  Planet(
+      image: "assets/images/planetgreen.png",
+      name: "EARTH",
+      history: "Lorem ipusm dolor si amet"),
+  Planet(
+      image: "assets/images/planetblue.png",
+      name: "VEUS",
+      history: "Lorem ipusm dolor si amet"),
+  Planet(
+      image: "assets/images/moon.jpg",
+      name: "MOON",
+      history: "Lorem ipusm dolor si amet"),
+  Planet(
+      image: "assets/images/mercury.jpg",
+      name: "MERCURY",
+      history: "Lorem ipusm dolor si amet"),
+];
