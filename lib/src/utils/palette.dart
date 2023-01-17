4c7b11482010545f0792f4e20c1f3a7d38d6ac54
baimamboukar <baimamboukar@gmail.com>
2023-01-17 19:24:33 +0100

    CosmoSense 1.0

---
+++ lib/src/utils/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' show Color;
+
+class Palette {
+  static Color primary = const Color(0xFF764AF1);
+  static Color secondary = const Color(0xFFF9C54B);
+  static Color tertiary = const Color(0xFF0E8BD3);
+  static Color scaffold = const Color(0xFF00032A);
+
+  static Color white = const Color(0xFFF9F9F9);
+  static Color black = const Color(0xFF000000);
+  static Color grey = const Color(0xFFA9A9A9);
+  static Color red = const Color(0xFFE74C3C);
+  static Color green = const Color(0xFF2ECC71);
+}
