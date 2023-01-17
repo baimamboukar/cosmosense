2565e549ea995deafb34abda6923ad36ddb2f36d
baimamboukar <baimamboukar@gmail.com>
2023-01-17 19:24:33 +0100

    CosmoSense 1.0

---
+++ lib/src/ui/screens/welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome'),
+      ),
+    );
+  }
+}
