f02f83bdefeb79e432f0dbfabd4a22077262c69d
baimamboukar <baimamboukar@gmail.com>
2023-01-17 19:24:33 +0100

    CosmoSense 1.0

---
+++ lib/src/utils/theme/light_theme.dart
@@ -0,0 +1,6 @@
+import 'package:cosmosense/src/utils/palette.dart';
+import 'package:cosmosense/src/utils/theme/dark_theme.dart';
+
+final lightTheme = darkTheme.copyWith(
+  primaryColor: Palette.primary,
+);
