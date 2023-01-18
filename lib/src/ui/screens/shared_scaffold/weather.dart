b1f6ff90941e9ce2fe6b452c4bcc283079caf22c
baimamboukar <baimamboukar@gmail.com>
2023-01-18 13:46:27 +0100

    CosmoSense 1.0

--- lib/src/ui/screens/shared_scaffold/weather.dart
+++ lib/src/ui/screens/shared_scaffold/weather.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cosmosense/src/ui/widgets/widgets.dart';
 import 'package:cosmosense/src/utils/palette.dart';
 import 'package:cosmosense/src/utils/textstyles/text_styles.dart';
@@ -17,14 +18,33 @@ class Weather extends ConsumerWidget {
               borderRadius: BorderRadius.circular(0),
             ),
             height: 300,
-            child: const Center(child: Text('Nasa Image'))),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60.0, left: 10),
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: Text(
+                          "MERCURY",
+                          style: Styles.title()
+                              .copyWith(fontFamily: "Daesang", fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
         Expanded(
           child: Row(
             children: [
               const Expanded(child: CircularListPage()),
               Container(
                 decoration: BoxDecoration(
-                  color: Colors.grey,
+                  color: Colors.transparent,
                   borderRadius: BorderRadius.circular(0),
                 ),
                 height: MediaQuery.of(context).size.height,
@@ -32,18 +52,29 @@ class Weather extends ConsumerWidget {
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          const Icon(Icons.wb_sunny),
-                          Text(
-                            "-145°F",
-                            style: Styles.decorateText(
-                                bold: true, color: Palette.secondary, size: 20),
-                          ),
-                        ],
-                      )
+                      WeatherInfoBox(
+                        index: 1,
+                        data: '-145°F',
+                        label: 'Temperature',
+                        iconPath: 'assets/images/uvindex.png',
+                        color: Palette.secondary,
+                      ),
+                      WeatherInfoBox(
+                        index: 2,
+                        data: '200Pa',
+                        label: 'Pressure',
+                        iconPath: 'assets/images/pressure.png',
+                        color: Palette.primary,
+                      ),
+                      WeatherInfoBox(
+                        index: 3,
+                        data: '200ml',
+                        label: 'Humidity',
+                        iconPath: 'assets/images/humidity.png',
+                        color: Palette.grey,
+                      ),
                     ],
                   ),
                 ),
@@ -52,6 +83,75 @@ class Weather extends ConsumerWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class WeatherInfoBox extends StatelessWidget {
+  final String data;
+  final String label;
+  final String iconPath;
+  final Color color;
+  final int index;
+  const WeatherInfoBox({
+    Key? key,
+    required this.data,
+    required this.label,
+    required this.iconPath,
+    required this.color,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideInRight(
+      //delay: const Duration(milliseconds: 250),
+      from: 200,
+      duration: Duration(milliseconds: index * 750),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            height: 100,
+            width: 120,
+            decoration: BoxDecoration(
+              color: color.withOpacity(0.65),
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          Positioned(
+            top: 40,
+            left: 10,
+            child: Text(
+              data,
+              style: TextStyle(
+                  fontSize: 18, fontFamily: "Varino", color: Palette.white),
+            ),
+          ),
+          Positioned(
+            bottom: 15,
+            left: 10,
+            child: Text(
+              label,
+              style: TextStyle(
+                  fontSize: 12,
+                  fontFamily: "Exo",
+                  color: Palette.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            top: -10,
+            right: -10,
+            child: Image.asset(
+              iconPath,
+              color: color,
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
