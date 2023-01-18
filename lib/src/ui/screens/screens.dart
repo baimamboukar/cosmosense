b93698f6a92b295e21b50cbfaf2760ae39e955f7
baimamboukar <baimamboukar@gmail.com>
2023-01-18 10:42:07 +0100

    CosmoSense 1.0

--- lib/src/ui/screens/screens.dart
+++ lib/src/ui/screens/screens.dart
@@ -3,7 +3,7 @@ library screens;
 export 'authentication/login.dart';
 export 'authentication/signin.dart';
 export 'shared_scaffold/cosmos.dart';
-export 'shared_scaffold/daily_image.dart';
+export 'shared_scaffold/astrobips.dart';
 export 'shared_scaffold/exploration.dart';
 export 'shared_scaffold/weather.dart';
 export 'welcome.dart';
