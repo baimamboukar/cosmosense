9ee7f760d81c4dac85ffc23d381a040725db5471
baimamboukar <baimamboukar@gmail.com>
2023-01-18 10:42:07 +0100

    CosmoSense 1.0

--- lib/src/ui/screens/shared_scaffold/weather.dart
+++ lib/src/ui/screens/shared_scaffold/weather.dart
@@ -1,3 +1,6 @@
+import 'package:cosmosense/src/ui/widgets/widgets.dart';
+import 'package:cosmosense/src/utils/palette.dart';
+import 'package:cosmosense/src/utils/textstyles/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,12 +9,49 @@ class Weather extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SingleChildScrollView(
-      child: Column(
-        children: const [
-          Text('Weather in space'),
-        ],
-      ),
+    return Column(
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(0),
+            ),
+            height: 300,
+            child: const Center(child: Text('Nasa Image'))),
+        Expanded(
+          child: Row(
+            children: [
+              const Expanded(child: CircularListPage()),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(0),
+                ),
+                height: MediaQuery.of(context).size.height,
+                width: 130,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Icon(Icons.wb_sunny),
+                          Text(
+                            "-145°F",
+                            style: Styles.decorateText(
+                                bold: true, color: Palette.secondary, size: 20),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
