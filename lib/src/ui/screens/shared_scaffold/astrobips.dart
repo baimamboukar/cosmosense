84eea508d695a7afa96672739703114cf5762414
baimamboukar <baimamboukar@gmail.com>
2023-01-18 10:42:06 +0100

    CosmoSense 1.0

---
+++ lib/src/ui/screens/shared_scaffold/astrobips.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NasaImage extends ConsumerWidget {
+  const NasaImage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Text("Astrobips");
+  }
+}
