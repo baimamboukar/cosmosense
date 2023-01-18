7313e8ca5ff337605a4a4f1581738461d043ba62
baimamboukar <baimamboukar@gmail.com>
2023-01-18 08:41:41 +0100

    CosmoSense 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:cosmosense/src/router/router.gr.dart' as routes;
 import 'package:cosmosense/src/router/router.gr.dart';
 import 'package:cosmosense/src/utils/palette.dart';
 import 'package:cosmosense/src/utils/theme/dark_theme.dart';
-import 'package:cosmosense/src/utils/theme/light_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:line_icons/line_icons.dart';
 
 Future<void> main() async {
@@ -34,7 +34,65 @@ class _CosmoSenseState extends ConsumerState<CosmoSense> {
       title: "CosmoSense",
       restorationScopeId: "cosmosense",
       debugShowCheckedModeBanner: false,
-      theme: lightTheme,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        textTheme: GoogleFonts.exoTextTheme(
+          const TextTheme(
+            bodyText1: TextStyle(color: Colors.white, fontFamily: "Exo"),
+          ).apply(bodyColor: Palette.white, displayColor: Palette.white),
+        ),
+        fontFamily: "Exo",
+        typography: Typography.material2021(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: const MaterialColor(0xFF143887, {
+          100: Color(0xFF143887),
+          700: Color(0xFFEEC36D),
+          600: Color(0xFF143887),
+          200: Color(0xFFEEC36D),
+          500: Color(0xFF143887),
+          400: Color(0xFFEEC36D),
+          50: Color(0xFF337669),
+          300: Color(0xFFEEC36D),
+        }),
+        primaryColor: Palette.primary,
+        scaffoldBackgroundColor: Palette.scaffold,
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          errorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+          ),
+          focusedErrorBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ),
       darkTheme: darkTheme,
       themeMode: ThemeMode.light,
     );
