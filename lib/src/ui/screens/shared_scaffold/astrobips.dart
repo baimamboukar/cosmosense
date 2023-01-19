cbbafbcbe3831a40d6ef9006b6f1e835af79a68e
baimamboukar <baimamboukar@gmail.com>
2023-01-19 15:51:41 +0100

    integrating NASA APOD

--- lib/src/ui/screens/shared_scaffold/astrobips.dart
+++ lib/src/ui/screens/shared_scaffold/astrobips.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:cosmosense/src/state/riverpods/apod_riverpod.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class NasaImage extends ConsumerWidget {
@@ -6,6 +7,31 @@ class NasaImage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text("Astrobips");
+    final apod = ref.watch(apodRiverpod);
+    return SafeArea(
+      child: apod.when(
+        loading: () {
+          return const Center(
+            child: CupertinoActivityIndicator(),
+          );
+        },
+        data: (apodData) {
+          return Column(
+            children: [
+              Text(apodData.copyright ?? ""),
+              Image.network(apodData.url ?? "image"),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(apodData.explanation ?? ""),
+              Text(apodData.date ?? ""),
+            ],
+          );
+        },
+        error: (error, stackTrace) {
+          return Text(error.toString());
+        },
+      ),
+    );
   }
 }
