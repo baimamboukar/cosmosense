18863dcd53862b40562fa08c60d77222380ac46a
baimamboukar <baimamboukar@gmail.com>
2023-01-19 08:01:09 +0100

    color animations

--- lib/src/utils/palette.dart
+++ lib/src/utils/palette.dart
@@ -11,4 +11,15 @@ class Palette {
   static Color grey = const Color(0xFFA9A9A9);
   static Color red = const Color(0xFFE74C3C);
   static Color green = const Color(0xFF2ECC71);
+
+  static Color mars = const Color(0xFFAC6119);
+  static Color neptune = const Color(0xFF4b70dd);
+
+  static Color earth = const Color(0xFF3B5D38);
+  static Color uranus = const Color(0xFFe1eeee);
+  static Color saturn = const Color(0xFF7B7869);
+  static Color mercury = const Color(0xFFe5e5e5);
+  static Color moon = const Color(0xFFC3C2BE);
+  static Color jupiter = const Color(0xFF404436);
+  static Color venus = const Color(0xFFFFC649);
 }
