befe142ccb1f994bff4b4829030dc1ecbe486e4d
baimamboukar <baimamboukar@gmail.com>
2023-01-20 17:43:21 +0100

    fixing list animations

--- lib/src/ui/widgets/animated_list.dart
+++ lib/src/ui/widgets/animated_list.dart
@@ -16,77 +16,119 @@ class FadeAnimatedList extends ConsumerStatefulWidget {
 
 class _FadeAnimatedListState extends ConsumerState<FadeAnimatedList> {
   final scrollController = ScrollController();
-
-  void onListen() {
-    setState(() {});
+  double topContainer = 0;
+  bool close = false;
+  void listenScroll() {
+    double firstEelementOffset = scrollController.offset / 80;
+    setState(() {
+      topContainer = firstEelementOffset;
+      close = scrollController.offset > 30;
+    });
   }
 
   @override
   void initState() {
-    widget.launches.addAll(List.from(widget.launches));
-    scrollController.addListener(onListen);
+    scrollController.addListener(listenScroll);
     super.initState();
   }
 
   @override
   void dispose() {
-    scrollController.removeListener(onListen);
+    scrollController.removeListener(listenScroll);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     const itemSize = 150.0;
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          const heightFactor = 0.6;
-          final launch = widget.launches[index];
-          final itemPositionOffset = index * itemSize * heightFactor;
-          final difference = scrollController.offset - itemPositionOffset;
-          final percent = 1.0 - (difference / (itemSize * heightFactor));
-          double opacity = percent;
-          double scale = percent;
-          if (opacity > 1.0) opacity = 1.0;
-          if (opacity < 0.0) opacity = 0.0;
-          if (percent > 1.0) scale = 1.0;
+    return ListView.builder(
+        controller: scrollController,
+        physics: const BouncingScrollPhysics(),
+        itemCount: widget.launches.length,
+        itemBuilder: (BuildContext context, int index) {
+          var launch = widget.launches[index];
 
-          return Align(
-            heightFactor: heightFactor,
-            child: Opacity(
-              opacity: opacity,
-              child: Transform(
-                alignment: Alignment.center,
-                transform: Matrix4.identity()..scale(scale, 1.0),
-                child: Card(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      topRight: Radius.circular(20.0),
-                    ),
-                  ),
-                  color: Palette.primary,
-                  child: SizedBox(
-                    height: itemSize,
-                    child: Row(
-                      children: <Widget>[
-                        const Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.all(15.0),
-                          ),
+          double scale = 1.0;
+          if (topContainer > 0.1) {
+            scale = index + 0.5 - topContainer;
+            if (scale < 0) {
+              scale = 0;
+            } else if (scale > 1) {
+              scale = 1;
+            }
+          }
+          return GestureDetector(
+            onTap: () {},
+            child: Align(
+              heightFactor: 0.6,
+              alignment: Alignment.topCenter,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: scale,
+                child: Transform(
+                  alignment: Alignment.bottomCenter,
+                  transform: Matrix4.identity()..scale(scale, 1.0),
+                  child: Card(
+                      color: Palette.moon,
+                      elevation: 5.0,
+                      // margin: const EdgeInsets.all(10.0),
+                      shadowColor: Palette.white,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(
+                                24.0,
+                              ),
+                              topRight: Radius.circular(24.0)),
+                          side: BorderSide(color: Colors.black12, width: 2.20)),
+                      child: SizedBox(
+                        height: 100.0,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, bottom: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(launch.name ?? "Launch AlphaX",
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              color: Palette.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16)),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10.0),
+                                  Row(
+                                    children: const [
+                                      Text("120 quizz"),
+                                      SizedBox(
+                                        width: 15.0,
+                                      ),
+                                      Text("10 Taken")
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Image.asset(
+                              launch.links!.patch!.small ??
+                                  "assets/icons/png/atom.png",
+                              height: 80.0,
+                              width: 80.0,
+                            ),
+                          ],
                         ),
-                        Image.network(launch.links!.patch!.small ??
-                            "https://images2.imgbox.com/6c/cb/na1tzhHs_o.png"),
-                      ],
-                    ),
-                  ),
+                      )),
                 ),
               ),
             ),
           );
-        },
-        childCount: widget.launches.length,
-      ),
-    );
+        });
   }
 }
