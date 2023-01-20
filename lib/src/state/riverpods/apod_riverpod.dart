5d045f4cc22d833f0f7f32fb12af939677a2b506
baimamboukar <baimamboukar@gmail.com>
2023-01-20 19:33:41 +0100

    going to details

--- lib/src/state/riverpods/apod_riverpod.dart
+++ lib/src/state/riverpods/apod_riverpod.dart
@@ -1,3 +1,4 @@
+import 'package:cosmosense/src/data/keys/nasa_api.dart';
 import 'package:cosmosense/src/data/models/apod.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,8 +6,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final apodRiverpod = FutureProvider<APOD>(
   (ref) async {
     final dio = Dio();
-    final data =
-        await dio.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+    final data = await dio
+        .get('https://api.nasa.gov/planetary/apod?api_key=$NASA_API_KEY');
     return APOD.fromJson(data.data);
   },
 );
