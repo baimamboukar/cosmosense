fcffcd11d7842fbf0e7afc5ac61ba2b393267178
baimamboukar <baimamboukar@gmail.com>
2023-01-20 18:35:35 +0100

    designing home page of launches

--- lib/src/ui/screens/shared_scaffold/exploration.dart
+++ lib/src/ui/screens/shared_scaffold/exploration.dart
@@ -1,5 +1,6 @@
 import 'package:cosmosense/src/state/riverpods/launches_riverpod.dart';
 import 'package:cosmosense/src/ui/widgets/widgets.dart';
+import 'package:cosmosense/src/utils/textstyles/text_styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,7 +19,26 @@ class Exploration extends ConsumerWidget {
         },
         data: (spaceXlaunches) {
           //final alphaLaunch = spaceXlaunches[10];
-          return FadeAnimatedList(launches: spaceXlaunches.take(15).toList());
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  RotatedBox(
+                    quarterTurns: 3,
+                    child: Text(
+                      "SPACEX LAUNCHES",
+                      style: Styles.title().copyWith(fontFamily: "Daesang"),
+                    ),
+                  ),
+                  Expanded(child: Image.asset("assets/images/spacexlaunch.jpg"))
+                ],
+              ),
+              Expanded(
+                  child: FadeAnimatedList(
+                      launches: spaceXlaunches.take(15).toList())),
+            ],
+          );
         },
         error: (error, stackTrace) {
           return Column(
