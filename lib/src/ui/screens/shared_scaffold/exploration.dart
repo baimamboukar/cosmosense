ad73049535890e96b955597d61bcd97605ab71bc
baimamboukar <baimamboukar@gmail.com>
2023-01-20 17:13:39 +0100

    adding fading list

--- lib/src/ui/screens/shared_scaffold/exploration.dart
+++ lib/src/ui/screens/shared_scaffold/exploration.dart
@@ -1,4 +1,5 @@
 import 'package:cosmosense/src/state/riverpods/launches_riverpod.dart';
+import 'package:cosmosense/src/ui/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,35 +10,25 @@ class Exploration extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final launches = ref.watch(spaceXlaunchesRiverpod);
     return SafeArea(
-      child: SingleChildScrollView(
-        child: launches.when(
-          loading: () {
-            return const Center(
-              child: CupertinoActivityIndicator(),
-            );
-          },
-          data: (spaceXlaunches) {
-            final alphaLaunch = spaceXlaunches[10];
-            return Column(
-              children: [
-                Image.network(alphaLaunch.links!.patch!.small ??
-                    "https://images2.imgbox.com/6c/cb/na1tzhHs_o.png"),
-                Text(alphaLaunch.name ?? ""),
-                Text(alphaLaunch.details ?? "no details"),
-                Text("Success: ${alphaLaunch.success}")
-              ],
-            );
-          },
-          error: (error, stackTrace) {
-            return Column(
-              children: [
-                Text(
-                  error.toString(),
-                ),
-              ],
-            );
-          },
-        ),
+      child: launches.when(
+        loading: () {
+          return const Center(
+            child: CupertinoActivityIndicator(),
+          );
+        },
+        data: (spaceXlaunches) {
+          //final alphaLaunch = spaceXlaunches[10];
+          return FadeAnimatedList(launches: spaceXlaunches.take(15).toList());
+        },
+        error: (error, stackTrace) {
+          return Column(
+            children: [
+              Text(
+                error.toString(),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
