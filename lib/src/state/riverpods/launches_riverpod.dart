e8dc029505e4b3ef849eeb77e09805854059081f
baimamboukar <baimamboukar@gmail.com>
2023-01-20 15:36:20 +0100

    launches REST API test

--- lib/src/state/riverpods/launches_riverpod.dart
+++ lib/src/state/riverpods/launches_riverpod.dart
@@ -1,10 +1,14 @@
+import 'package:cosmosense/src/data/models/spacex_launch.dart';
 import 'package:cosmosense/src/data/services/dio_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final spaceXlaunchesRiverpod = FutureProvider<String>(
+final spaceXlaunchesRiverpod = FutureProvider<List<SpaceXlaunch>>(
   (ref) async {
     final dio = ref.watch(dioProvider);
     final data = await dio.get('https://api.spacexdata.com/v4/launches/');
-    return data.toString();
+    // loop trough data and serialize
+    return data.data
+        .map<SpaceXlaunch>((launch) => SpaceXlaunch.fromJson(launch))
+        .toList();
   },
 );
