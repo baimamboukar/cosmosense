8447a183f7792173e6867b28c2a7742635c83598
baimamboukar <baimamboukar@gmail.com>
2023-01-23 01:26:54 +0100

    details refinements

--- lib/src/ui/screens/launch_details.dart
+++ lib/src/ui/screens/launch_details.dart
@@ -74,10 +74,12 @@
 // }
 
 import 'package:cosmosense/src/data/models/spacex_launch.dart';
+import 'package:cosmosense/src/state/riverpods/rocket_riverpod.dart';
+import 'package:cosmosense/src/ui/widgets/widgets.dart';
 import 'package:cosmosense/src/utils/palette.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:iconly/iconly.dart';
 import 'package:line_icons/line_icons.dart';
 
 class LaunchDetails extends ConsumerWidget {
@@ -86,83 +88,152 @@ class LaunchDetails extends ConsumerWidget {
   const LaunchDetails({super.key, required this.launch});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final rocketData =
+        ref.watch(rocketRiverpod(launch.rocket ?? "5e9d0d95eda69955f709d1eb"));
     return Scaffold(
       backgroundColor: Palette.background,
       body: Column(
         children: [
           Expanded(
-            child: CustomScrollView(
-              slivers: [
-                HeaderWidget(launch: launch),
-                const SliverToBoxAdapter(
-                  child: SizedBox(height: 40),
+            child: rocketData.when(
+              error: (error, stackTrace) => Center(
+                child: Text(
+                  error.toString(),
                 ),
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              launch.name ?? "No name",
-                              style: TextStyle(
-                                color: Palette.secondary,
-                                fontSize: 22,
-                                fontFamily: "Daesang",
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 4,
-                            ),
-                            const Text(
-                              "Launched by SpaceX",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Stack(
-                          fit: StackFit.loose,
-                          clipBehavior: Clip.none,
-                          children: [
-                            Container(
-                              height: MediaQuery.of(context).size.height * 0.3,
-                              width: MediaQuery.of(context).size.width * 0.35,
-                              padding: const EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                color: Palette.moon,
-                                borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(50)),
-                              ),
-                              child: Text(
-                                "Watch",
+              ),
+              loading: () => const Center(
+                child: CupertinoActivityIndicator(),
+              ),
+              data: (data) => CustomScrollView(
+                slivers: [
+                  HeaderWidget(launch: launch),
+                  const SliverToBoxAdapter(
+                    child: SizedBox(height: 40),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                launch.name ?? "No name",
                                 style: TextStyle(
-                                  color: Palette.moon,
-                                  fontSize: 12,
+                                  color: Palette.secondary,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                            Positioned(
-                              top: -30,
-                              left: -25,
-                              child: Image.network(
-                                launch.links!.patch!.small ?? "",
-                                height: 100,
-                                width: 100,
+                              const SizedBox(
+                                height: 4,
                               ),
-                            ),
-                          ],
-                        )
-                      ],
+                              Row(
+                                children: const [
+                                  Text(
+                                    "Launched by SpaceX",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Badge(
+                                    success: true,
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(LineIcons.fulcrum),
+                                  Text(
+                                    "Engine type",
+                                    style: TextStyle(
+                                      color: Palette.secondary,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(data.engines!.type ?? "No type"),
+                            ],
+                          ),
+                          Stack(
+                            fit: StackFit.loose,
+                            clipBehavior: Clip.none,
+                            children: [
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.3,
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  color: Palette.moon,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(50),
+                                    bottomRight: Radius.circular(50),
+                                    topRight: Radius.circular(10),
+                                    bottomLeft: Radius.circular(10),
+                                  ),
+                                ),
+                                child: Text(
+                                  "Watch",
+                                  style: TextStyle(
+                                    color: Palette.red,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                top: -30,
+                                left: -25,
+                                child: Image.network(
+                                  launch.links!.patch!.small ?? "",
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Palette.black,
+                child: Icon(
+                  LineIcons.rocket,
+                  size: 28,
+                  color: Palette.white,
                 ),
-              ],
+              ),
+              const SizedBox(
+                width: 2,
+              ),
+              const Text(
+                "Launch details",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SelectableText(
+            launch.details ?? "No details",
+            style: const TextStyle(
+              fontSize: 12,
             ),
           ),
           const BottomWidget(),
@@ -236,21 +307,22 @@ class HeaderWidget extends StatelessWidget {
             Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'Avicii',
-                      style: Theme.of(context).textTheme.headline4!.copyWith(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      "${launch.name} Images",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     CircleAvatar(
-                      backgroundColor: Palette.secondary,
+                      backgroundColor: Palette.background.withOpacity(0.5),
                       child: const Icon(
-                        IconlyLight.heart,
+                        Icons.fullscreen,
                         color: Colors.white,
                       ),
                     )
