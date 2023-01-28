65f3e73dbf9f979da4295233156dd59046c3e0cf
baimamboukar <baimamboukar@gmail.com>
2023-01-28 20:41:37 +0100

    presentation modifs

--- lib/src/ui/screens/authentication/signin.dart
+++ lib/src/ui/screens/authentication/signin.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Signin extends ConsumerWidget {
-  const Signin({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(child: Text('Signin')),
-    );
-  }
-}
+// singin screen with fogot password option
