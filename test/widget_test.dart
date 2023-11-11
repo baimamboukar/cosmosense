3ed2fce4ffb6b60781ade547cb4d3ba06ffc4e1b
baimamboukar <baimamboukar@gmail.com>
2023-11-11 21:32:33 +0100

    format: fix linters problems

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cosmosense/src/app/app.dart';
+import 'package:cosmosense/src/app/theme/cosmosense_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cosmosense/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Cosmosense widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const CosmoSense());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that MaterialApp.router is created.
+    final materialAppFinder = find.byType(MaterialApp);
+    expect(materialAppFinder, findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the theming system.
+    final MaterialApp materialApp = tester.widget(materialAppFinder);
+    expect(materialApp.theme, equals(CosmosenseTheme.light));
+    expect(materialApp.darkTheme, equals(CosmosenseTheme.dark));
+    expect(materialApp.themeMode, equals(ThemeMode.system));
+    expect(materialApp.routerConfig, isNotNull);
+    expect(materialApp.routeInformationParser, isNull);
   });
 }
